9b8da163c0a0ea7e1b82d67489354c1eaf83b972
jg-developer <jonathan.gomes213@gmail.com>
2020-10-05 13:06:24 -0300

    create and update user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,4 +6,27 @@ module.exports = {
 
     return res.json(results);
   },
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex("users").insert({ username });
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async update(req, res, next) {
+    try {
+      const { username } = req.body;
+      const { id } = req.params;
+
+      await knex("users").update({ username }).where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 const UserController = require("./controllers/UserController");
 
 routes.get("/users", UserController.index);
+routes.post("/users", UserController.create);
+routes.put("/users/:id", UserController.update);
 
 module.exports = routes;
